e74a8e7ea2b63f1e3b53b4a9ac91f0f095d5cb9f
Alexander-Oni <alex.evant1448@gmail.com>
2025-10-05 16:37:00 +0300

    Added methods for initializing and clearing the console

--- backend/user_interface.py
+++ backend/user_interface.py
@@ -3,3 +3,16 @@ import os
 
 # Инициализация цветного вывода в консоли
 init(autoreset=True)
+
+class UserInterface:
+  def __init__(self, db_manager):
+    """
+    Конструктор - принимает объект для работы с базой данных
+    """
+    self.db = db_manager
+  
+  def clear_screen(self):
+    """
+    Очищает экран консоли 
+    """
+    os.system('cls' if os.name == 'nt' else 'clear')
