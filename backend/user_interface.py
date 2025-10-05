aadde841d28726933a3dbdecb89abc29b2790e32
Alexander-Oni <alex.evant1448@gmail.com>
2025-10-05 16:52:23 +0300

    Added methods for displaying main menu

--- backend/user_interface.py
+++ backend/user_interface.py
@@ -37,4 +37,48 @@ class UserInterface:
 
     input(Fore.YELLOW + "\n Нажмите Enter для продолжения...")
 
+  def main_menu(self):
+    """
+    Главное меню системы
+    """
+
+    while True:
+      self.print_header("ГЛАВНОЕ МЕНЮ")
+      
+      # Показываем все доступные опции
+      print(Fore.WHITE + "1.  Управление пользователями")
+      print(Fore.WHITE + "2.  Управление категориями")
+      print(Fore.WHITE + "3.  Управление ключевыми словами")
+      print(Fore.WHITE + "4.  Просмотр статистики")
+      print(Fore.WHITE + "5.  Быстрая блокировка контента")
+      print(Fore.RED + "0.  Выход из программы")
+      print()
+      
+      # Получаем выбор пользователя
+      choice = input(Fore.GREEN + " Выберите действие (0-5): ")
+      
+      # Обрабатываем выбор
+      if choice == "1":
+        self.users_menu()
+
+      elif choice == "2":
+        self.categories_menu()
+
+      elif choice == "3":
+        self.keywords_menu()
+
+      elif choice == "4":
+        self.statistics_menu()
+
+      elif choice == "5":
+        self.quick_block_menu()
+
+      elif choice == "0":
+        print(Fore.YELLOW + " До свидания!")
+        break
+
+      else:
+        print(Fore.RED + " Неверный выбор! Попробуйте снова.")
+        self.wait_for_enter()
+
   
