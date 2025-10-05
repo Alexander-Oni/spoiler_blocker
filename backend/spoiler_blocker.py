6f932e5d75dad3bde143a034ca8ab8ff33708ed0
Alexander-Oni <alex.evant1448@gmail.com>
2025-10-05 21:52:37 +0300

    Added welcome massege in main file

---
+++ backend/spoiler_blocker.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+SPOILER BLOCKER - Система блокировки спойлеров
+Главный файл программы
+
+Автор: Онищенко Алксандр
+Группа: 243-323
+Дата: 05.10.2025
+"""
+
+# Импортируем необходимые модули
+from db_manager import DatabaseManager
+from user_interface import UserInterface
+from colorama import Fore, Style, init
+import sys
+
+# Инициализируем библиотеку для цветного вывода
+init(autoreset=True)
+
+def main():
+  """
+  Главная функция программы - точка входа в приложение
+  """
+
+  # Выводим красивый заголовок
+  print(Fore.CYAN + """
+  ********************************************
+  *         SPOILER BLOCKER v0.01            *
+  *   Система блокировки спойлеров           *
+  ********************************************
+  """)
+  
+  print(Fore.YELLOW + "         Система блокировки спойлеров v0.01")
+  print(Fore.CYAN + "=" * 50)
+  print()
+    
+# Точка входа в программу
+if __name__ == "__main__":
+  main()
