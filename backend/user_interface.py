7892b120d878d977df293fcd7f836d2e4407e49a
Alexander-Oni <alex.evant1448@gmail.com>
2025-10-05 16:42:50 +0300

    Added methods for displaying header and waiting for input

--- backend/user_interface.py
+++ backend/user_interface.py
@@ -9,10 +9,32 @@ class UserInterface:
     """
     Конструктор - принимает объект для работы с базой данных
     """
+
     self.db = db_manager
   
   def clear_screen(self):
     """
     Очищает экран консоли 
     """
+
     os.system('cls' if os.name == 'nt' else 'clear')
+
+  def print_header(self, title):
+    """
+    Выводит красивый заголовок 
+    """
+
+    self.clear_screen()
+    print(Fore.CYAN + "=" * 60)
+    print(Fore.CYAN + f"SPOILER BLOCKER - {title}")
+    print(Fore.CYAN + "=" * 60)
+    print()
+    
+  def wait_for_enter(self):
+    """
+    Ожидает нажатия клавиши Enter для продолжения
+    """
+
+    input(Fore.YELLOW + "\n Нажмите Enter для продолжения...")
+
+  
